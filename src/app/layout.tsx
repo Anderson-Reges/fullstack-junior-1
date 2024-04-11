f234194244c45a46b44b2b6385ccccddb89ee06f
Anderson-Reges <andersonreges76@gmail.com>
2024-04-11 18:33:22 -0300

    feat: creating the background rings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="relative">
+        <main className="relative h-svh overflow-hidden">
           {children}
         </main>
       </body>
