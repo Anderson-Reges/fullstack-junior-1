88a6f6f9628c0952eca0260dda9bc3a0291d0663
Anderson-Reges <andersonreges76@gmail.com>
2024-04-11 14:00:02 -0300

    fix: restoring jobs.ts

--- src/data/jobs.ts
+++ src/data/jobs.ts
@@ -1,35 +1,26 @@
 export const jobs = [
-     
-    {
-      id: 1,
-      job: 'Full Stack Developer',
-      level: 'Junior',
-      status: 'open'
-    }, 
-    {
-      id: 2,
-      job: 'Frontend Developer',
-      level: 'Junior',
-      status: 'closed'
-    }, 
-    {
-      id: 3,
-      job: 'Backend Developer',
-      level: 'Junior',
-      status: 'closed'
-    }, 
-    {
-      id: 4,
-      job: 'Full Stack Developer',
-      level: 'Senior',
-      status: 'closed'
-    }, 
-    {
-      id: 5,
-      job: 'undefined',
-      level: 'undefined',
-      status: 'undefined'
-    }
-      
-]
-    
+  {
+    id: 1,
+    job: 'Full Stack Developer',
+    level: 'Junior',
+    status: 'open',
+  },
+  {
+    id: 2,
+    job: 'Frontend Developer',
+    level: 'Junior',
+    status: 'closed',
+  },
+  {
+    id: 3,
+    job: 'Backend Developer',
+    level: 'Junior',
+    status: 'closed',
+  },
+  {
+    id: 4,
+    job: 'Full Stack Developer',
+    level: 'Senior',
+    status: 'closed',
+  },
+];
