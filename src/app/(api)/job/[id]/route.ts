8e16c082d3fc05b0803774cbd270222426032d88
Anderson-Reges <andersonreges76@gmail.com>
2024-04-11 14:11:09 -0300

    feat: creating the route retrieve a job by id

--- src/app/(api)/job/[id]/route.ts
+++ src/app/(api)/job/[id]/route.ts
@@ -1,11 +1,21 @@
+import { jobs } from "@/data/jobs";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
-  req: NextRequest,
-  res: NextResponse
+  _req: NextRequest,
+  { params }: { params: { id: string } },
 ): Promise<NextResponse> {
-  return NextResponse.json(
-    {status: "up"},
-    {status: 200}
+  const id = params.id
+  
+  const isExist = jobs.filter(job => job.id === Number(id))
+
+  if (isExist.length === 0) return NextResponse.json(
+    { error: `Job with ID ${id} not found` },
+    { status: 404 }
   )
+
+  return NextResponse.json(
+    isExist[0],
+    { status: 200 }
+  ) 
 }
