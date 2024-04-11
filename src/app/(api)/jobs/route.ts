6d1dea53b6807ef741e0c5906ea3b12710655ced
Anderson-Reges <andersonreges76@gmail.com>
2024-04-11 12:27:26 -0300

    feat: implementing jobs route

--- src/app/(api)/jobs/route.ts
+++ src/app/(api)/jobs/route.ts
@@ -1,11 +1,24 @@
-import { NextRequest, NextResponse } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
+import { jobs } from "../../../../data/jobs";
 
 export async function GET(
   req: NextRequest,
   res: NextResponse
 ): Promise<NextResponse> {
+  const searchParams = req.nextUrl.searchParams
+  const query = searchParams.get('level')
+
+  if (query) {
+    const filteredJobs = jobs.filter(({level}) => query === level)
+
+    return NextResponse.json(
+      filteredJobs,
+      {status: 200}
+    )
+  }
+
   return NextResponse.json(
-    {status: "up"},
+    jobs,
     {status: 200}
   )
 }
